8eba19f8887c9744624890cd86d55d2ad2abe14a
Ym111 <ym_chen95@outlook.com>
2020-04-01 15:20:14 -0700

    class version

---
+++ github-finder/src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h1>About This APP</h1>
+            <p>App to seatch Github</p>
+        </div>
+    )
+}
+export default About
